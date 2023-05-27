fe4d817a2fca027f872eb62bc85a62e8c5103353
simon hu <simon.hu.1999@gmail.com>
2023-05-28 00:46:45 +0200

    Added tom-select controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,8 @@ application.register("hello", HelloController)
 import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
 
+import TomSelectController from "./tom_select_controller"
+application.register("tom-select", TomSelectController)
+
 import TypedJsController from "./typed_js_controller"
 application.register("typed-js", TypedJsController)
