bab2035705e2e4230fe49ade99a61c237fda76e0
simon hu <simon.hu.1999@gmail.com>
2023-05-28 02:22:35 +0200

    Added star rating system

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :list
 
-  validates :rating, presence: true
+  validates :rating, presence: true, inclusion: 0..5
   validates :comment, length: { minimum: 6, too_short: "%{count} characters is the minimum allowed"}
 end
